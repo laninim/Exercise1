83cb6b3644096f6d299ac3b74c5509c39504c25a
Mauro Lanini <mallettomachico@gmail.com>
2022-11-01 15:46:43 +0100

    Initial Commit

---
+++ src/it/develhope/Exercise1/TestProgrammers.java
@@ -0,0 +1,22 @@
+package it.develhope.Exercise1;
+
+public class TestProgrammers {
+
+    public static void main(String[] args) {
+        Programmer Mauro = new Programmer();
+        Mauro.name = "Mauro";
+        Mauro.age = 32;
+        Mauro.wearsGlasses = false;
+
+        Programmer jimmy = new Programmer();
+        jimmy.name = "Jimmy";
+        jimmy.age = 45;
+        jimmy.wearsGlasses = true;
+
+        Mauro.drinkCoffee();
+        Mauro.printDetails();
+
+        jimmy.printDetails();
+        jimmy.hasGlasses();
+    }
+}
